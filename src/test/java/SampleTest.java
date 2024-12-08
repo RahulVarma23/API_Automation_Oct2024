c7c859455fb4b4d4882940ac939c783f13dce57a
rahul <rahulvarma411@gmail.com>
2024-12-08 10:28:19 +0530

    adding new file

---
+++ src/test/java/SampleTest.java
@@ -0,0 +1,19 @@
+import io.restassured.response.Response;
+import model.Employee;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class SampleTest {
+
+    @Test
+    public void testPatchRequest() {
+        String BASE_URI = "https://reqres.in/api/users";
+
+        Employee employee = new Employee("laxman", "qa" , 20);
+
+        Response response = given().baseUri(BASE_URI).body(employee).log().all().patch().then().extract().response();
+
+        System.out.println("status code: "+response.statusCode());
+    }
+}
